973ac7e9897d790641a8204c224c1ebf58f43a79
ManojXworkz36 <manojkb.xworkz@gmail.com>
2023-08-22 11:33:42 +0530

    for examples

---
+++ Factorial.java
@@ -0,0 +1,19 @@
+class Factorial{
+public static void main(String arfs[]){
+int factNum = 5;
+int dummy = 1;
+for(int factNumber=1;factNumber<=factNum;factNumber++){
+dummy= dummy*factNumber;
+
+}
+
+System.out.println("the factorial of " + factNum  + "   is    " + dummy);
+
+
+
+}
+
+
+
+
+}
