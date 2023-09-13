bb05c4514cc25349e1a1742f471a7fd7e664a13f
ManojXworkz36 <manojkb.xworkz@gmail.com>
2023-09-13 22:05:09 +0530

    Examples

---
+++ Patient.java
@@ -0,0 +1,105 @@
+public class Patient{
+
+  private int patientId;
+  private String patientName;
+   private int patientAge;
+   private String  diseaseName;
+   private  int wardNo;
+   private String admittedDate;
+   private String hospitalName;
+   private String patientAdress;
+   private String  patientGender;
+   private String consultantDoctor;
+   
+   public void setPatientId(int patientId){
+	   
+	   this.patientId = patientId;
+   }
+public int getPatientId(){
+
+ return patientId;	
+}
+
+public void setPatientName(String patientName){
+	   
+	   this.patientName = patientName;
+   }
+public String getPatientName(){
+
+ return patientName;	
+}
+
+public void setPatientAge(int patientAge){
+	   
+	   this.patientAge = patientAge;
+   }
+public int getPatientAge(){
+
+ return patientAge;	
+}
+ public void setDiseaseName(String diseaseName){
+	   
+	   this.diseaseName = diseaseName;
+   }
+public String getDiseaseName(){
+
+ return diseaseName;	
+}
+public void setWardNo(int wardNo){
+	   
+	   this.wardNo = wardNo;
+   }
+public int getWardNo(){
+
+ return wardNo;	
+}
+public void setAdmittedDate(String admittedDate){
+	   
+	   this.admittedDate = admittedDate;
+   }
+public String getAdmittedDate(){
+
+ return admittedDate;	
+}
+ 
+ public void setHospitalName(String hospitalName){
+	   
+	   this.hospitalName = hospitalName;
+   }
+public String getHospitalName(){
+
+ return hospitalName;	
+}
+
+ public void setPatientAdress(String patientAdress){
+	   
+	   this.patientAdress = patientAdress;
+   }
+public String getPatientAdress(){
+
+ return patientAdress;	
+}
+
+ public void setPatientGender(String patientGender){
+	   
+	   this.patientGender = patientGender;
+   }
+public String getPatientGender(){
+
+ return patientGender;	
+}
+
+ public void setConsultantDoctor(String consultantDoctor){
+	   
+	   this.consultantDoctor = consultantDoctor;
+   }
+public String getConsultantDoctor(){
+
+ return consultantDoctor;	
+}
+
+
+
+
+
+}
