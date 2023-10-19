4845c8c7ac691551e11265373429149d4f589c37
ManojXworkz36 <manojkb.xworkz@gmail.com>
2023-10-19 11:36:46 +0530

    crud

---
+++ police-station/src/com/xworkz/policeapp/policestation/Officer.java
@@ -0,0 +1,21 @@
+package com.xworkz.policeapp.policestation;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Officer {
+    private String officerName;
+    private int officerId;
+    private String officerAddress;
+    private int officerAge;
+    private String officerPostName;
+    private String officerBloodroup;
+    private String officerGender;
+
+}
